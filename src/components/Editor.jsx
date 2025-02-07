a0982a6147a9ad0df72117a9d0d05af29fc00766
KyungTaeP <qkrrudxo96@gmail.com>
2025-02-07 10:17:35 +0900

    [feat]List 삭제 기능 추가

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,40 +1,41 @@
-import "./Editor.css"
-import { useState, useRef } from "react" 
+import { useRef, useState } from "react";
+import "./Editor.css";
 
-const Editor = ({onCreate}) => {
-    const [content, setContent] = useState("")
-    const contentRef = useRef()
+const Editor = ({ onCreate }) => {
+  const [content, setContent] = useState("");
+  const inputRef = useRef();
 
-    const onChangeContent = (e) => {
-        setContent(e.target.value)
+  const onChangeContent = (e) => {
+    setContent(e.target.value);
+  };
+
+  const onKeydown = (e) => {
+    if (e.keyCode === 13) {
+      onSubmit();
     }
+  };
 
-    const onSubmit = () => {
-        if(content === ""){
-            contentRef.current.focus()
-            return;
-        }
-        onCreate(content)
-        setContent("")
+  const onSubmit = () => {
+    if (content === "") {
+      inputRef.current.focus();
+      return;
     }
+    onCreate(content);
+    setContent("");
+  };
 
-    const onKeydonw = (e) => {
-        if(e.keyCode === 13) {
-            onSubmit()
-        }
-    }
+  return (
+    <div className="Editor">
+      <input
+        ref={inputRef}
+        value={content}
+        onChange={onChangeContent}
+        onKeyDown={onKeydown}
+        placeholder="새로운 Todo..."
+      />
+      <button onClick={onSubmit}>추가</button>
+    </div>
+  );
+};
 
-    return(
-        <div className="Editor">
-            <input
-                ref={contentRef} 
-                value={content} 
-                onKeyDown={onKeydonw}
-                onChange={onChangeContent} 
-                placeholder="새로운 Todo..." />
-            <button onClick = { onSubmit }>추가</button>
-        </div>
-    )
-}
-
-export default Editor
+export default Editor;
